32fd2e2d51ecfd7e9b8c90272dbdcb0e76a85711
batenzar <batenzar@gmail.com>
2020-04-06 20:03:07 +0700

    Add items service

--- flask-restful/flask-restful-app.py
+++ flask-restful/flask-restful-app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -14,10 +14,19 @@ class Item(Resource):
         return {'msg': 'Item not found'}, 404 # 404 is the http code that you want python to see.
 
     def post(self, name):
+        data = request.get_json()
+        #data = request.get_json(force=True) # ignore content-type in header, it always converted to json
+        #data = request.get_json(silent=True) # do not throw error when parsing failed. Just display empty page.
         item = {'name' : name, 'price': 12.00}
         items.append(item)
         return item, 201 # 201 - Created Successful, 202 - Create request accepted. (Migth not be created immediatly)
 
-api.add_resource(Item, '/item/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+# app.run(port=5000)
+app.run(port=5000, debug=True) # For display error page for debugging
