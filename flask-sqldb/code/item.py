7ba14488f42a08479b8261f0cedb5850d41f4610
batenzar <batenzar@gmail.com>
2020-04-09 00:25:45 +0700

    Modify put method to use database

--- flask-sqldb/code/item.py
+++ flask-sqldb/code/item.py
@@ -70,14 +70,31 @@ class Item(Resource):
 
     def put(self, name):
         data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name':name,'price':data['price']}
+
         if item is None:
-            # insert
-            item = {'name':name,'price':data['price']}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {"msg": "Inserting error"}, 500 
         else:
-            item.update(data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                return {"msg": "Updating error"}, 500 
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
 
 class ItemList(Resource):
     def get(self):
