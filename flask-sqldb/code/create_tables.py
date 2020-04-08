6c66bc64da5516eb452e1d13df446812a87814dd
batenzar <batenzar@gmail.com>
2020-04-08 21:51:13 +0700

    Implement function to initial db

---
+++ flask-sqldb/code/create_tables.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+# create auto incrementing id primary key as int
+# Note: you can't use 'id int' here. It must use full name so we use 'id INTERGER'
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+cursor.execute(create_table)
+
+connection.commit()
+connection.close()
