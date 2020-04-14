0baa25d681affdeaa4cb71d95583667221314a18
batenzar <batenzar@gmail.com>
2020-04-15 00:25:11 +0700

    Add login endpoint which point to UserLogin class

--- flask-tokenrefreshing/code/flask-restful-app-withauthen.py
+++ flask-tokenrefreshing/code/flask-restful-app-withauthen.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -11,7 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True # Flask will returns the specific error to match Exception. If we don't specific the code 500 always be returned.
-app.secret_key = 'jose'
+app.secret_key = 'jose' # app.config['JWT_SECRET_KEY']
 api = Api(app)
 
 
@@ -28,6 +27,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     from db import db
